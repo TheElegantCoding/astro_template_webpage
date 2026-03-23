e93edfbb0b50fc4b750397040b2a7aa36c39a7dd
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-23 13:07:26 -0400

    refactor: enhance StructuredDataWebsiteType with potentialAction and alternateName properties

--- src/global/type/structured_data/structured_data_website_type.ts
+++ src/global/type/structured_data/structured_data_website_type.ts
@@ -1,9 +1,16 @@
-type StructuredDataWebsiteType<T> =
-{
+type StructuredDataSearchAction = {
+  target: string;
+  'query-input': string;
+  '@type': 'SearchAction';
+};
+
+type StructuredDataWebsiteType<T> = {
   url: string;
   name: string;
   mainEntity?: T[];
   '@type': 'WebSite';
+  alternateName?: string[];
+  potentialAction?: StructuredDataSearchAction;
 };
 
 export type {
