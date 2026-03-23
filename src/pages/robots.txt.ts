500b865dc153b3c39ff842fd3bb4ef4ad5e11093
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-23 12:55:12 -0400

    refactor: rename and enhance robots.txt response handling

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,15 +1,21 @@
 import type { APIRoute } from 'astro';
 
-const getRobotsTxt = (sitemapURL: URL) => `
+const robotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
 
 Sitemap: ${sitemapURL.href}
-`;
+`.trim();
 
 export const GET: APIRoute = ({ site }) =>
 {
   const sitemapURL = new URL('sitemap-index.xml', site);
 
-  return new Response(getRobotsTxt(sitemapURL));
+  return new Response(robotsTxt(sitemapURL), {
+    headers: {
+      'Cache-Control': 'public, max-age=3600',
+      'Content-Type': 'text/plain; charset=utf-8'
+    },
+    status: 200
+  });
 };
