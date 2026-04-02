c97eccff411a76f6195b792551b0abfd56a5c9fa
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-01 23:08:07 -0400

    refactor: enhance astro.config.ts with astroPWA integration and CSP directives

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
+import astroPWA from '@vite-pwa/astro';
 import { classShortener } from 'astro-class-shortener';
 import { inlineCss } from 'astro-inline-css';
 import { defineConfig } from 'astro/config';
@@ -17,10 +18,29 @@ export default defineConfig({
   integrations: [
     sitemap({ lastmod: new Date() }),
     partytown({ config: { forward: ['gtag', 'dataLayer.push'] } }),
-    inlineCss({ prefixPath: environment.BASE_URL }),
-    classShortener()
+    inlineCss({
+      prefixPath: environment.BASE_URL,
+      csp: true,
+      cspHashPlaceholder: 'sha256-INLINE_CSS_HASH'
+    }),
+    classShortener(),
+    astroPWA({
+      base: environment.BASE_URL,
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,avif}'],
+        navigateFallback: '/404'
+      }
+    })
   ],
-  security: { csp: true },
+  security: {
+    csp: {
+      directives: ["object-src 'none'"],
+      styleDirective: {
+        hashes: ['sha256-INLINE_CSS_HASH']
+      }
+    }
+  },
   output: 'static',
   server: {
     host: true,
