ff453409aad55b12903acfbd5aab55c60c381870
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-01 23:10:59 -0400

    refactor: add readonly modifier to properties in StructuredDataContactPoint, StructuredDataQuantitativeValue, and StructuredDataOrganization types

--- src/global/type/structured_data/structured_data_organization_type.ts
+++ src/global/type/structured_data/structured_data_organization_type.ts
@@ -1,39 +1,39 @@
 import type { StructuredDataPostalAddressType } from '@global/type/structured_data/structured_data_postal_address_type';
 
 type StructuredDataContactPoint = {
-  email: string;
-  telephone: string;
-  '@type': 'ContactPoint';
+  readonly email: string;
+  readonly telephone: string;
+  readonly '@type': 'ContactPoint';
 };
 
 type StructuredDataQuantitativeValue = {
-  value?: number;
-  minValue?: number;
-  maxValue?: number;
-  '@type': 'QuantitativeValue';
+  readonly value?: number;
+  readonly minValue?: number;
+  readonly maxValue?: number;
+  readonly '@type': 'QuantitativeValue';
 };
 
 type StructuredDataOrganization = {
-  url: string;
-  name: string;
-  duns?: string;
-  email?: string;
-  naics?: string;
-  vatID?: string;
-  leiCode?: string;
-  sameAs?: string[];
-  legalName?: string;
-  telephone?: string;
-  foundingData?: Date;
-  description: string;
-  iso6523Code?: string;
-  alternateName?: string;
-  logo: string | string[];
-  '@type': 'Organization';
-  globalLocationNumber?: string;
-  contactPoint?: StructuredDataContactPoint;
-  address?: StructuredDataPostalAddressType[];
-  numberOfEmployees?: StructuredDataQuantitativeValue;
+  readonly url: string;
+  readonly name: string;
+  readonly duns?: string;
+  readonly email?: string;
+  readonly naics?: string;
+  readonly vatID?: string;
+  readonly leiCode?: string;
+  readonly sameAs?: string[];
+  readonly legalName?: string;
+  readonly telephone?: string;
+  readonly foundingData?: Date;
+  readonly description: string;
+  readonly iso6523Code?: string;
+  readonly alternateName?: string;
+  readonly logo: string | string[];
+  readonly '@type': 'Organization';
+  readonly globalLocationNumber?: string;
+  readonly contactPoint?: StructuredDataContactPoint;
+  readonly address?: StructuredDataPostalAddressType[];
+  readonly numberOfEmployees?: StructuredDataQuantitativeValue;
 };
 
 export type {
