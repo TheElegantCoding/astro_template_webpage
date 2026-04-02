f7c2b5777935c762bc092ca94fe31438b6857b75
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-02 13:00:17 -0400

    refactor: restructure GET function in rss.xml.ts for improved readability

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,12 @@
 import rss from '@astrojs/rss';
 import { siteConfiguration } from '@global/configuration/site_configuration';
 
-export const GET = async () => rss({
-  description: siteConfiguration.description,
-  items: [],
-  site: import.meta.env.BASE_URL,
-  title: siteConfiguration.siteName,
-  trailingSlash: false
-});
+export const GET = async () => {
+  return rss({
+    description: siteConfiguration.description,
+    items: [],
+    site: import.meta.env.BASE_URL,
+    title: siteConfiguration.siteName,
+    trailingSlash: false
+  });
+};
