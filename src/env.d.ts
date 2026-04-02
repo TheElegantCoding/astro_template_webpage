e0b51f41c3c16d6be3b9edde99a9d5d957370f6e
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-02 13:00:09 -0400

    feat: add PWA support by declaring modules for pwa-info and pwa-register

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,38 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="vite-pwa/info" />
+/// <reference types="vite-pwa/client" />
+
 type ImportMetaEnv = {
   readonly PORT: number;
   readonly BASE_URL: string;
   readonly MODE: 'staging' | 'production' | 'development';
 };
+
+declare module 'virtual:pwa-info' {
+  export const pwaInfo: undefined | {
+    webManifest: {
+      href: string;
+      linkTag: string;
+    };
+  };
+}
+
+declare module 'virtual:pwa-register' {
+  type RegisterSWOptions = {
+    immediate?: boolean;
+    onNeedRefresh?: () => void;
+    onOfflineReady?: () => void;
+    onRegisterError?: (error: any) => void;
+    onRegistered?: (registration: undefined | ServiceWorkerRegistration) => void;
+  };
+
+  function registerSW (options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>;
+
+  export {
+    registerSW,
+    RegisterSWOptions
+  };
+}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
