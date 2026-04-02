27ba5def96307bb4919052c4eb5d1c5467029b02
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-01 23:09:48 -0400

    refactor: streamline type definition for BaseLayoutProps in base_layout_type.ts

--- src/global/type/base_layout_type.ts
+++ src/global/type/base_layout_type.ts
@@ -1,8 +1,7 @@
 import type { MetaDataProps } from '@global/component/meta_data.astro';
 import type { StructuredDataProps } from '@global/type/structured_data/structured_data_props';
 
-type BaseLayoutProps<T> =
-{
+type BaseLayoutProps<T> = {
   readonly metaData?: MetaDataProps;
   readonly structuredData?: StructuredDataProps<T>;
 };
