1830e910d4411de8fd9131433268a60d1281bad5
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-02 13:00:04 -0400

    refactor: improve file path handling in findAllFiles function and add excluded keywords in image conversion

--- script/conversion/image.ts
+++ script/conversion/image.ts
@@ -5,9 +5,21 @@ const convertImage = () => {
   const publicImageFiles = findAllFiles('.png');
   const sourceImageFiles = findAllFiles('.png', '/src');
 
+  const excludedKeywords = [
+    'pwa',
+    'web-app-manifest',
+    'apple-touch',
+    'opg',
+    'favicon'
+  ];
+
   const image = [...sourceImageFiles, ...publicImageFiles];
 
   image.forEach(async (file) => {
+    if (excludedKeywords.some((keyword) => { return file.includes(keyword); })) {
+      return;
+    }
+
     await sharp(file)
       .webp({ quality: 80 })
       .toFile(file.replace('.png', '.webp'));
--- script/util/file.ts
+++ script/util/file.ts
@@ -2,10 +2,11 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 const findAllFiles = (extension: string, directory = 'public') => {
-  const filesPath = directory;
+  const root = process.cwd();
+  const filesPath = path.join(root, directory);
   const resultFiles = fs.readdirSync(filesPath, { recursive: true });
-  const filterFiles = resultFiles.filter((element) => element.includes(extension.toLowerCase()));
-  const result = filterFiles.map((element) => path.join(directory, element.toString()));
+  const filterFiles = resultFiles.filter((element) => { return element.includes(extension.toLowerCase()); });
+  const result = filterFiles.map((element) => { return path.join(directory, element.toString()); });
 
   return result;
 };
