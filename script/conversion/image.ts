0437976ba1cd44b977a58142700b8e63f71e9078
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-01 21:23:43 -0400

    refactor: improve formatting and spacing in image conversion script

--- script/conversion/image.ts
+++ script/conversion/image.ts
@@ -1,15 +1,13 @@
 import { findAllFiles } from '@script/util/file';
 import sharp from 'sharp';
 
-const convertImage = () =>
-{
+const convertImage = () => {
   const publicImageFiles = findAllFiles('.png');
   const sourceImageFiles = findAllFiles('.png', '/src');
 
-  const image = [ ...sourceImageFiles, ...publicImageFiles ];
+  const image = [...sourceImageFiles, ...publicImageFiles];
 
-  image.forEach(async (file) =>
-  {
+  image.forEach(async (file) => {
     await sharp(file)
       .webp({ quality: 80 })
       .toFile(file.replace('.png', '.webp'));
