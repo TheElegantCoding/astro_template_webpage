fc2a2ab2ffb68013bafc68daf45bac107d27f7d3
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-15 21:53:03 -0400

    chore: restructure site configuration for improved organization and clarity

--- src/global/configuration/site_configuration.ts
+++ src/global/configuration/site_configuration.ts
@@ -1,17 +1,36 @@
-import { pwaConfiguration } from './pwa_configuration';
-
 const siteConfiguration = {
-  author: {
-    email: 'astralysoftware@gmail.com',
-    name: 'Astralys Software',
-    web: 'astralysoftware.com'
+  metaAuthor: {
+    author: {
+      email: 'astralysoftware@gmail.com',
+      name: 'Astralys Software',
+      web: 'astralysoftware.com'
+    },
+    copyright: `© ${new Date().getFullYear()} Astralys Software`
   },
-  copyright: `© ${new Date().getFullYear()} Astralys Software`,
-  defaultLanguage: 'es',
+  i18n: {
+    defaultLanguage: 'es',
+    languages: ['es']
+  },
+  openGraph: {
+    siteName: 'Astralight',
+    type: 'website',
+    image: {
+      alt: 'Astralight',
+      height: 630,
+      url: '/asset/opg/opg.png',
+      secureImageUrl: '/asset/opg/opg.png',
+      width: 1200
+    }
+  },
+  favicon: {
+    apple: '/asset/favicon/apple-touch-icon.png',
+    ico: '/asset/favicon/favicon.ico',
+    manifest: '/asset/favicon/site.webmanifest',
+    png: '/asset/favicon/favicon.png',
+    svg: '/asset/favicon/favicon.svg'
+  },
   description: '',
-  googleAnalyticId: '',
-  languages: { es: 'Español' },
-  ...pwaConfiguration
+  googleAnalyticId: ''
 };
 
 export { siteConfiguration };
