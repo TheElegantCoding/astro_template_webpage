38c5436de467fccbf0009916d36c31f765d68af4
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-15 21:50:01 -0400

    fix: refactor astro.config.ts by removing unused imports and simplifying integrations

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,14 @@
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
-import astroPWA from '@vite-pwa/astro';
 import { classShortener } from 'astro-class-shortener';
+import { compress } from 'astro-crush';
 import { inlineCss } from 'astro-inline-css';
 import { defineConfig } from 'astro/config';
 import { resolve } from 'node:path';
 
-import { environmentData } from './script/validation/environment';
-import { siteConfiguration } from './src/global/configuration/site_configuration';
+import { environment } from './src/global/configuration/environment_configuration';
 
-const { BASE_URL, PORT, NODE_ENV: ENV } = environmentData;
+const { BASE_URL, PORT, NODE_ENV: ENV } = environment;
 const dirname = resolve();
 
 export default defineConfig({
@@ -18,57 +17,20 @@ export default defineConfig({
     inlineStylesheets: 'never'
   },
   integrations: [
-    sitemap({ lastmod: new Date() }),
     partytown({
       config: {
         forward: ['gtag', 'dataLayer.push'],
         debug: ENV === 'development'
       }
     }),
+    classShortener(),
     inlineCss({
       prefixPath: BASE_URL,
       csp: true,
       cspHashPlaceholder: 'sha256-INLINE_CSS_HASH'
     }),
-    classShortener(),
-    astroPWA({
-      base: ENV === 'production' ? `${BASE_URL}/` : '/',
-      injectRegister: 'script',
-      registerType: 'autoUpdate',
-      disable: true,
-      manifest: {
-        name: siteConfiguration.siteName,
-        short_name: siteConfiguration.shortName,
-        description: siteConfiguration.description,
-        orientation: siteConfiguration.orientation,
-        start_url: ENV === 'production' ? BASE_URL : '/',
-        theme_color: siteConfiguration.themeColor,
-        background_color: siteConfiguration.backgroundColor,
-        lang: siteConfiguration.defaultLanguage,
-        display: siteConfiguration.display,
-        icons: siteConfiguration.icons,
-        screenshots: siteConfiguration.screenshots
-      },
-      devOptions: {
-        enabled: ENV === 'development',
-        type: 'module',
-        navigateFallback: 'index.html'
-      },
-      workbox: {
-        globIgnores: [
-          '**/404.html',
-          '404.html',
-          '404'
-        ],
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-        navigateFallback: '/index.html',
-        runtimeCaching: [{
-          urlPattern: /\/404/,
-          handler: 'NetworkOnly'
-        }],
-        importScripts: ['https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js']
-      }
-    })
+    sitemap({ lastmod: new Date() }),
+    compress()
   ],
   security: {
     csp: {
@@ -92,19 +54,6 @@ export default defineConfig({
   trailingSlash: 'never',
   vite: {
     envDir: './src/global/env',
-    build: {
-      assetsInlineLimit: 0,
-      rollupOptions: {
-        output: {
-          manualChunks (id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-            return null;
-          }
-        }
-      }
-    },
     css: {
       preprocessorOptions: {
         scss: { loadPaths: [resolve(dirname, 'src')] }
