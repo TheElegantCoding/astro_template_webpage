3bf4572c9517fee5aa2cf57967be3937b3a0fadd
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-15 21:52:57 -0400

    chore: add environment configuration for managing application settings

---
+++ src/global/configuration/environment_configuration.ts
@@ -0,0 +1,20 @@
+import { createEnvironment } from 'envlys';
+import { z } from 'zod';
+
+const environmentTypes = z.object({
+  NODE_ENV: z.enum(['development', 'production'], {
+    error: "NODE_ENV has to be either 'development' or 'production'"
+  }),
+  PORT: z.coerce.number({ error: 'PORT must be a number (e.g., 4321)' }),
+  BASE_URL: z.string({
+    error: 'Base URL is required and must be a string (e.g., http://localhost:4321)'
+  }).default('http://localhost:4321')
+});
+
+const environment = createEnvironment(environmentTypes, {
+  path: './src/global/env',
+  generateExample: true,
+  listEnvironment: ['development', 'production']
+});
+
+export { environment };
